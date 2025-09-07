12627aeb2b9b7cd9abef021539cfba3484992a98
menachem yarhi <nbjo460@gmail.com>
2025-09-07 11:38:36 +0300

    func get metadata

---
+++ src/config.py
@@ -0,0 +1,7 @@
+WAV_FILES_PATH = r"C:\Users\Menachem\Desktop\Galil\Muazin\data\podcasts"
+
+class MetadataConfig:
+    SIZE = "size"
+    NAME = "name"
+    CREATION_DATE = "creation date"
+    LAST_MODIFICATION = "last modification"
---
+++ src/manager.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from config import MetadataConfig
+import config
+
+class Manager:
+    def __init__(self):
+        self.podcasts_path = config.WAV_FILES_PATH
+    def run(self):
+        self._control_metadata()
+
+    def _control_metadata(self):
+        podcasts_list = self._get_list_of_files()
+        for podcast in podcasts_list:
+            self._get_metadata_on_file(podcast)
+
+
+
+    def _get_list_of_files(self):
+        """
+        return list of all podcasts path
+        :return: list
+        """
+        podcasts_list = list(Path(self.podcasts_path).iterdir())
+        return podcasts_list
+
+    def _get_metadata_on_file(self, file_path : Path):
+        metadata = {}
+        file_stat = file_path.stat()
+        metadata[MetadataConfig.SIZE] = file_stat.st_size
+        metadata[MetadataConfig.CREATION_DATE] = file_stat.st_ctime
+        metadata[MetadataConfig.LAST_MODIFICATION] = file_stat.st_mtime
+        metadata[MetadataConfig.NAME] = file_path.name
+
+        return metadata
+
