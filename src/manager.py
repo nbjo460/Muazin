082aa8077bf0ac7cb8351aec0c7aea37a3fa9ba5
menachem yarhi <nbjo460@gmail.com>
2025-09-07 12:09:23 +0300

    changed metedata to readible the dates

--- src/manager.py
+++ src/manager.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from config import MetadataConfig
+from datetime import datetime
+
 import config
 import json
 
@@ -14,6 +16,7 @@ class Manager:
         for podcast_path in podcasts_list:
             metadata = self._get_metadata_on_file(podcast_path)
             json_file_info = self._merge_metadata_and_path_to_json(metadata, podcast_path)
+            print(json_file_info)
 
 
     def _merge_metadata_and_path_to_json(self, metadata : dict, file_path : Path):
@@ -45,10 +48,15 @@ class Manager:
         """
         metadata = {}
         file_stat = file_path.stat()
+
+
         metadata[MetadataConfig.SIZE] = file_stat.st_size
-        metadata[MetadataConfig.CREATION_DATE] = file_stat.st_ctime
-        metadata[MetadataConfig.LAST_MODIFICATION] = file_stat.st_mtime
+        metadata[MetadataConfig.CREATION_DATE] = Manager._convert_timestamp_to_datetime(file_stat.st_ctime)
+        metadata[MetadataConfig.LAST_MODIFICATION] = Manager._convert_timestamp_to_datetime(file_stat.st_mtime)
         metadata[MetadataConfig.NAME] = file_path.name
 
         return metadata
 
+    @staticmethod
+    def _convert_timestamp_to_datetime(timestamp):
+        return str(datetime.fromtimestamp(timestamp))
