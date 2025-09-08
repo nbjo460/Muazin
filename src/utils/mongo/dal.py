20fb0cd1a95adebd942a83c5060fe3b54c4b6435
menachem yarhi <nbjo460@gmail.com>
2025-09-08 11:08:23 +0300

    fixed bug file xisted error

--- src/utils/mongo/dal.py
+++ src/utils/mongo/dal.py
@@ -1,8 +1,7 @@
 from pymongo import MongoClient
-from gridfs import GridFSBucket
+from gridfs import GridFSBucket, errors
 from pathlib import Path
 from src.utils.config import MongoDBConfig
-from bson import ObjectId
 
 class DAL:
     def __init__(self):
@@ -35,9 +34,12 @@ class DAL:
 
     def upload_file(self, file_path, _id):
         file_name = Path(file_path).name
-        with open(file_path, 'rb') as file:
-            with self.fs.open_upload_stream_with_id(_id, filename=file_name) as fs_stream:
-                fs_stream.write(file)
+        try:
+            with open(file_path, 'rb') as file:
+                with self.fs.open_upload_stream_with_id(_id, filename=file_name) as fs_stream:
+                    fs_stream.write(file)
+        except errors.FileExists:
+            print("FILE ALREADY EXISTS, NOT UPLOADED AGAIN")
 
 # if __name__ == "__main__":
 #     dal = DAL()
