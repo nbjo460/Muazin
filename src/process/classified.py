87bb2793329c5859ae2ca31be790f635bc745049
menachem yarhi <nbjo460@gmail.com>
2025-09-09 17:59:02 +0300

    all funcs exceed alculateor words

--- src/process/classified.py
+++ src/process/classified.py
@@ -1,5 +1,6 @@
 import base64
 from src.utils.config import ClassifiedConfig
+from collections import Counter
 class Classified:
     def __init__(self):
         hostile_words_encoded = ClassifiedConfig.HOSTILE_WORDS
@@ -10,15 +11,28 @@ class Classified:
 
         print(self.hostile_words, self.less_hostile_words)
 
+    def classified(self, text : str):
+        danger_percents = self.calculate_percents()
+        is_bds = self.is_indicted(danger_percents)
+        bds_threat_level = self.threat_level(is_bds, danger_percents)
+
     def calculate_percents(self):
         bds_percent = None
         return bds_percent
-    def is_bds_classified(self):
-        is_bds = None
-        return is_bds
-    def threat_level(self):
-        bds_threat_level = None
-        return bds_threat_level
+
+    @staticmethod
+    def is_indicted(danger_percents : float) -> bool:
+        if danger_percents > ClassifiedConfig.INDICTED_LEVEL: return True
+        else: return False
+
+    @staticmethod
+    def threat_level(_is_bds, _danger_percents):
+         if _is_bds:
+             if _danger_percents > ClassifiedConfig.HIGH_LEVEL: return ClassifiedConfig.HIGH
+             else: return ClassifiedConfig.MEDIUM
+         return ClassifiedConfig.NONE
+
+
 
     @staticmethod
     def decrypt_base64_string(raw_str : str):
@@ -27,4 +41,17 @@ class Classified:
         decoded_string = decoded_bytes.decode("utf-8")
         return decoded_string
 
-a = Classified()
+    def study(self):
+        listi1 = ["12345", "12345", "12345", "23456", "67890"]
+        listi2 = ["12345", "67890", "aaaaa", "bbbbb"]
+        a = list(set(listi1) & set(listi2))
+        counter1 = Counter(listi1)
+        sum = {}
+        for part in a:
+            count1 = counter1.get(part, 0)
+            sum[part] = count1
+        print(sum)
+
+if __name__ == "__main__":
+    a = Classified()
+    a.study()
